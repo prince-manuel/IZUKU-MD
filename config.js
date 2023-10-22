94b9157ab4300a58ee0f65237610a355f98dfa70
Linus Brathake <108346317+Lino7902@users.noreply.github.com>
2023-10-22 10:10:44 +0300

    Update config.js

--- config.js
+++ config.js
@@ -7,12 +7,12 @@ global.owner = process.env.OWNER_NUMBER.split(",")
 global.mongodb = process.env.MONGODB_URI || "mongodb+srv://sam:sam@cluster0.u1smxsv.mongodb.net/?retryWrites=true&w=majority"
 global.port= process.env.PORT || 5000
 global.email = 'sam@secktor.live'
-global.github = 'https://github.com/excelottah6/IZUKU-MD'
+global.github = 'https://github.com/Lino7902/IZUKU-MD'
 global.location = 'Sultanpur IN'
-global.gurl = 'https://instagram.com/' // add your username
-global.sudo = process.env.SUDO || '2347039570336,923466319114'
-global.devs = '2347039570336';
-global.website = 'https://github.com/excelottah6/IZUKU-Md' //wa.me/+91000000000000
+global.gurl = 'https://instagram.com/kin_sstreet_?igshid=OGU0MmVlOWVjOQ==' // add your username
+global.sudo = process.env.SUDO || '254768276341,254737444458'
+global.devs = '254768276341';
+global.website = 'https://github.com/Lino7902/IZUKU-Md' //wa.me/+254768276341
 global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://raw.githubusercontent.com/SecktorBot/Brandimages/main/logos/SocialLogo%201.png'
 module.exports = {
   botname:   process.env.BOT_NAME === undefined ? 'IZUKU 𝐁𝐨𝐭' : process.env.BOT_NAME,
